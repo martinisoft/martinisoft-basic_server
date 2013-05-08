9bc6f1a8d052d447cb37bc32d0cc8eb792ac5e7a
Aaron Kalin <akalin@martinisoftware.com>
2013-05-07 23:33:32 -0500

    Initial commit

---
+++ metadata.rb
@@ -0,0 +1,13 @@
+name             "martinisoft-basic_server"
+maintainer       "Aaron Kalin"
+maintainer_email "akalin@martinisoftware.com"
+license          "Apache 2.0"
+description      "Basic server role for Martini Software"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.1.0"
+
+supports "ubuntu"
+
+%w{ ubuntu build-essential chef-client openssl openssh user }.each do |cb|
+  depends cb
+end
