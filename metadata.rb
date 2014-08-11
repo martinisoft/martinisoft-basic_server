27dbffbca543aef2d57bf15cee6b84842f5a250e
Aaron Kalin <akalin@martinisoftware.com>
2014-08-10 23:48:21 -0500

    Update description

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ name             "martinisoft-server"
 maintainer       "Aaron Kalin"
 maintainer_email "akalin@martinisoftware.com"
 license          "Apache 2.0"
-description      "A cookbook that configures a given server in the way Martini Software expects"
+description      "A cookbook that configures any given server for Martini Software"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "1.0.0"
 
