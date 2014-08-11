a7e4183ad3f72a89049faa1c93a83886f4e68201
Aaron Kalin <akalin@martinisoftware.com>
2014-08-11 01:21:40 -0500

    Swap quotes

--- metadata.rb
+++ metadata.rb
@@ -1,16 +1,16 @@
-name             "martinisoft-server"
-maintainer       "Aaron Kalin"
-maintainer_email "akalin@martinisoftware.com"
-license          "Apache 2.0"
-description      "A cookbook that configures any given server for Martini Software"
+name             'martinisoft-server'
+maintainer       'Aaron Kalin'
+maintainer_email 'akalin@martinisoftware.com'
+license          'Apache 2.0'
+description      'A cookbook that configures any given server for Martini Software'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          IO.read(File.join(File.dirname(__FILE__), 'VERSION')) rescue '0.0.1'
 
-supports "ubuntu", ">= 12.04"
+supports 'ubuntu', '>= 12.04'
 
-depends "build-essential", "~> 2.0.4"
-depends "martinisoft-chef-client"
-depends "martinisoft-openssh", "~> 1.0.0"
-depends "openssl", "~> 1.1.0"
-depends "ubuntu", "~> 1.1.6"
-depends "users", "~> 1.7.0"
+depends 'build-essential', '~> 2.0.4'
+depends 'martinisoft-chef-client'
+depends 'martinisoft-openssh', '~> 1.0.0'
+depends 'openssl', '~> 1.1.0'
+depends 'ubuntu', '~> 1.1.6'
+depends 'users', '~> 1.7.0'
