0c97869523cd9f980dff9092db9c5a90e44a2805
Aaron Kalin <akalin@martinisoftware.com>
2014-08-28 00:28:16 -0500

    Resolve test integration issues
    When conducting a search Chef ignores the provided config and
    accidentally loads the new client configuration which causes a 401 error
    to occur.

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -18,4 +18,16 @@ describe 'A Martini Software Server' do
     expect(command('hostname -f')).to return_stdout('walle.martinisoftware.com')
   end
 
+  describe 'has a "martinisoft" user' do
+
+    it 'that exists' do
+      expect(user('martinisoft')).to exist
+    end
+
+    it 'that belongs to the "admin" group' do
+      expect(user('martinisoft')).to belong_to_group('admin')
+    end
+
+  end
+
 end
